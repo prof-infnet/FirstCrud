30919869024ad4bd8eb19f22b856058e62d7622f
jonh.carvalho <jonh.carvalho@prof.infnet.edu.br>
2022-08-23 07:47:28 -0300

    Adicionado Controller Index1

--- FirstCrud/Controllers/HomeController.cs
+++ FirstCrud/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace FirstCrud.Controllers
         {
             return View();
         }
+        public IActionResult Index1()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
